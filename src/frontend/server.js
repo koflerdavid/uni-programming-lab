4e66484579e9f4947a822305c01b68b06ab64880
David Kofler <kofler.david@gmail.com>
2015-04-14 01:24:00 +0200

    Typo

--- src/frontend/server.js
+++ src/frontend/server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var port = 8080;
-console.log("Starting server on 'locahost:"+port+"'");
+console.log("Starting server on 'localhost:"+port+"'");
 connect().use(serveStatic(__dirname)).listen(8080);
