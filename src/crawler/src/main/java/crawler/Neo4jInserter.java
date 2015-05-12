5c29b0c071b5dc326d045f25c122c98ae7f2a6f6
David Kofler <kofler.david@gmail.com>
2015-05-12 10:07:34 +0200

    Improvements: Now only a URI is necessary to activate a crawler

--- src/crawler/src/main/java/crawler/Neo4jInserter.java
+++ src/crawler/src/main/java/crawler/Neo4jInserter.java
@@ -194,7 +194,7 @@ public class Neo4jInserter {
 
         graphDb.execute("MATCH (team:Team) RETURN team.uri AS uri").forEachRemaining(row -> {
             Team team = new Team((String) row.get("uri"), (String) row.get("name"));
-            teamCrawler.crawlTeamPage(team);
+            teamCrawler.crawlTeamPage(team.getUri(), team);
         });
     }
 
@@ -220,7 +220,7 @@ public class Neo4jInserter {
         TournamentCrawler tournamentCrawler = getTournamentCrawler(teamCrawler);
 
         try {
-            tournamentCrawler.crawlTournamentPage(tournament);
+            tournamentCrawler.crawlTournamentPage(tournament.getUri(), tournament);
             return tournament;
 
         } catch (IOException e) {
@@ -239,7 +239,7 @@ public class Neo4jInserter {
                             "RETURN team.uri as teamUri")
                     .columnAs("teamUri");
 
-            incompleteTeams.forEachRemaining(teamUri -> teamCrawler.crawlTeamPage(new Team(teamUri, "")));
+            incompleteTeams.forEachRemaining(teamCrawler::crawlTeamPage);
 
             tx.success();
         }
