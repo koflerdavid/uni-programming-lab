be81591c870bf9251c4719f3d71887256d77a4d0
David Kofler <kofler.david@gmail.com>
2015-05-12 10:03:50 +0200

    Neo4j importer (albeit not a very useful one)

---
+++ src/crawler/src/main/java/crawler/CypherImporter.java
@@ -0,0 +1,31 @@
+package crawler;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.QueryExecutionException;
+import org.neo4j.graphdb.Transaction;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CypherImporter {
+    public static void main(String... args) {
+        GraphDatabaseService graphDb = Neo4jHelper.openGraphDb(args[0]);
+        String file = args[1];
+
+        Neo4jHelper.createSchema(graphDb);
+
+        try (Transaction tx = graphDb.beginTx()) {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            reader.lines().forEachOrdered(line -> {
+                System.out.println(line);
+                graphDb.execute(line);
+            });
+
+            tx.success();
+        } catch (IOException | QueryExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
